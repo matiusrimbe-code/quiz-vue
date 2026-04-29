1648a1fa6f648affaac28ec3742ffc0e5ec7ce69
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2026-04-29 22:19:47 +0800

    learn read data JSON, watchers, create component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,64 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue'
+import dataQuiz from './data/quizes.json'
+import QuizCard from './components/QuizCard.vue'
+const quizes = ref(dataQuiz)
+const search = ref('')
+
+watch(search, () => {
+  quizes.value = dataQuiz.filter((quiz) => {
+    return quiz.title.toLowerCase().includes(search.value.toLowerCase())
+  })
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <div class="header">
+      <h1>Quizes</h1>
+      <input v-model.trim="search" type="text" class="search-input" placeholder="Search ..." />
+    </div>
+
+    <div class="content">
+      <QuizCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  max-width: 900px;
+  width: 100%;
+  min-height: 100vh;
+  margin: 0 auto;
+  background-color: #fff;
+  padding: 20px;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  gap: 40px;
+  margin: 40px;
+  border-bottom: 1px solid #cfcfcf;
+  padding-bottom: 20px;
+
+  border-top: 1px solid #cfcfcf;
+  padding-top: 20px;
+}
+
+.search-input {
+  padding: 10px 20px;
+  border-radius: 20px;
+  font-size: 16px;
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+  flex-wrap: wrap;
+  margin: 40px;
+}
+</style>
---
+++ src/components/QuizCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+const { quiz } = defineProps(['quiz'])
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-header">
+      <img :src="quiz.img" :alt="quiz.title" width="230px" />
+    </div>
+    <div class="card-body">
+      <h3>{{ quiz.title }}</h3>
+      <p>{{ quiz.questions.length }} Questions</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid #cfcfcf;
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+.card-body {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
